c069f1a3ef29bee1d553bb171dfb094fe1e3fe4e
jamarshon <jasonl96@gmail.com>
2018-12-29 01:14:55 -0500

    aa

--- graph/dijkstra_simple.cpp
+++ graph/dijkstra_simple.cpp
@@ -25,12 +25,28 @@ using namespace std;
 
 typedef long long ll;
 
-unordered_map<int, int> dijkstra(unordered_map<int, vector<int>>& adj, int source) {
-    typedef pair<int,int> pii;
+typedef pair<int,int> pii;
+unordered_map<int, int> dijkstra(unordered_map<int, vector<pii>>& adj, int source) {
     unordered_map<int, int> dist;
     for(const auto& u_adj: adj) dist[u_adj.first] = INT_MAX;
     priority_queue<pii, vector<pii>, greater<pii>> pq; // dist, edge and gives smallest dist first. 
+
     pq.emplace(0, source);
+    dist[source] = 0;
+
+    while(!pq.empty()) {
+        pii d_to_u = pq.top();
+        pq.pop();
+        if(d_to_u.first > dist[d_to_u.second]) continue;
+
+        for(const auto& v: adj[d_to_u.second]) {
+            if(d_to_u.first + v.second < dist[v.first]) {
+                dist[v.first] = d_to_u.first + v.second;
+                pq.emplace(dist[v.first], v.first);
+            }
+        }
+    }
+
     return dist;
 }
 
